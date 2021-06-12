f56878f151e96a17f93c0471188cbdc1fbaa928f
APereira <A07pereira@gmail.com>
2021-06-11 23:11:21 -0400

    final push

---
+++ assets/js/questions.js
@@ -0,0 +1,41 @@
+var questions = [{
+    question: "What tag is used to define a container for an external app or plug-in?",
+    option1: "<embed>",
+    option2: "<caption>",
+    option3: "<!DOCTYPE>",
+    option4: "<code>",
+    answer: "0"
+  },
+  {
+    question: "What tag is required in all HTML documents, and is used to define the title?",
+    option1: "<title></title>",
+    option2: "<head></head>",
+    option3: "<br></br>",
+    option4: "<body></body>",
+    answer: "0"
+  },
+  {
+    question: "Which HTML tag us used to define an internal style sheet",
+    option1: "<style>",
+    option2: "<css>",
+    option3: "<script>",
+    option4: "<js>",
+    answer: "0"
+  },
+  {
+    question: "Which HTML attribute is used to define inline styles?",
+    option1: "font",
+    option2: "class",
+    option3: "styles",
+    option4: "style",
+    answer: "3"
+  },
+  {
+    question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+    option1: "Boolean",
+    option2: "Condition",
+    option3: "RegExp",
+    option4: "Event",
+    answer: "0"
+  }
+];
